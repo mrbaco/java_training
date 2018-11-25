2b6fb391f628ef31ca3110b2fdd712bf7ebf4fe6
mrbaco <mrbaco@ya.ru>
2018-11-25 17:45:00 +0300

    Задание №2. Класс Point

---
+++ sandbox/src/main/java/ru/testqa/my/Point.java
@@ -0,0 +1,16 @@
+package ru.testqa.my;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Point pair) {
+    double var = (this.x - pair.x) * (this.x - pair.x) + (this.y - pair.y) * (this.y - pair.y);
+    return Math.sqrt(var);
+  }
+}
--- sandbox/src/main/java/ru/testqa/my/my.java
+++ sandbox/src/main/java/ru/testqa/my/my.java
@@ -2,7 +2,19 @@ package ru.testqa.my;
 
 public class my {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    System.out.println(2 + 2);
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(1, 1);
+
+    System.out.println("-- ФУНКЦИЯ --");
+    System.out.println("Расстояние между двумя точками: " + distance(p1, p2));
+
+    System.out.println("-- МЕТОД --");
+    System.out.println("Расстояние между двумя точками: " + p1.distance(p2));
+    System.out.println("Расстояние между двумя точками (alt): " + p2.distance(p1));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    double var = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+    return Math.sqrt(var);
   }
 }
