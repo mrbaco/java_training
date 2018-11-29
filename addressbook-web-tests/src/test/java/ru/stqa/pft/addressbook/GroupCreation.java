d4312d2cb18bc1056619dfd699de86658d7eb7bc
mrbaco <mrbaco@ya.ru>
2018-11-29 23:44:34 +0300

    Параметризованные методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreation.java
@@ -13,29 +13,52 @@ public class GroupCreation {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
     wd.get("http://localhost/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    gotoGroupsPage();
+    initGroupCreation();
+    fillGroupCreationForm(new GroupData("new group", "Чив-чив", "Футер"));
+    submitGroupCreationForm();
+    returnToGroupCreationPage();
+  }
+
+  private void returnToGroupCreationPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreationForm() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupCreationForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("new group");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("Чив-чив");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("Футер");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupsPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
