52e6ffb1ca2b041ac26f12723ee2300b89584cc5
mrbaco <mrbaco@ya.ru>
2018-11-30 00:02:02 +0300

    Задание №4. Создание контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,91 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String mobile;
+  private final String email;
+  private final String homepage;
+  private final String bday;
+  private final String bmonth;
+  private final String byear;
+  private final String notes;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String mobile, String email, String homepage, String bday, String bmonth, String byear, String notes) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+    this.homepage = homepage;
+    this.bday = bday;
+    this.bmonth = bmonth;
+    this.byear = byear;
+    this.notes = notes;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getBday() {
+    return bday;
+  }
+
+  public String getBmonth() {
+    return bmonth;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
