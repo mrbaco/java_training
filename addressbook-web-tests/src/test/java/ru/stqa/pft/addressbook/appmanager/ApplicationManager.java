358fb71257a3909681fe23e560212a6f98a5e048
mrbaco <mrbaco@ya.ru>
2018-12-12 00:13:06 +0300

    Задание №7. Запуск тестов в разных браузерах
    Запуск проверен для каждого из 3х драйверов, исполняемые файлы драйверов хранятся в отдельной директории на локальной машине

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,17 @@ public class ApplicationManager {
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) wd = new FirefoxDriver();
+    else if (browser == BrowserType.CHROME) wd = new ChromeDriver();
+    else wd = new InternetExplorerDriver();
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/");
 
